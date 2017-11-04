ff21ee6d04c7d12f8e8f2ba05f8264da97059272
Santi Nuñez <tau150@hotmail.com>
2017-11-04 19:38:14 -0300

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import API_KEY_WEATHER from '../api_keys';
+
+
+
+
+const API_KEY = API_KEY_WEATHER;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=metric`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},ar`
+  const request = axios.get(url);
+
+  console.log(  request);
+  return{
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
---
+++ src/api_keys.js
@@ -0,0 +1,4 @@
+
+const API_KEY_WEATHER = 'ed7b34986576a0b179ebbcefd31ade9a';
+
+export default API_KEY_WEATHER;
